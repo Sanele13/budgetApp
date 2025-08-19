003bad91d1177847fb6487993d34f4f7d4e42d46
Sanele Mpangalala <sanele@rodcode.co.za>
2025-08-19 21:04:39 +0200

    added functionality to create expense and view expense list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Expense } from '../models/expense.model';
+import { ModalController } from '@ionic/angular';
+import { ExpenseComponent } from '../expense/expense.component';
 
 @Component({
   selector: 'app-tab1',
@@ -8,12 +11,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  expenses: any[] = [];
+  expenses: Expense[] = [];
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   addExpense() {
-    throw new Error('Method not implemented.');
+    this.modalCtrl.create({
+      component: ExpenseComponent,
+      componentProps: {
+        expense: {
+          id: Date.now(),
+          description: '',
+          amount: 0,
+          date: new Date()
+        },
+        budgetItems: [
+          { id: 1, description: 'Groceries', amount: 300 },
+          { id: 2, description: 'Transport', amount: 100 },
+          { id: 3, description: 'Entertainment', amount: 150 }
+        ]
+      }
+    }).then(modal => {
+      modal.present();
+
+      modal.onDidDismiss().then(result => {
+        if (result.data) {
+          this.expenses.push(result.data);
+
+          console.log('Expense added:', result.data);
+        }
+      });
+    });
   }
 
 }
