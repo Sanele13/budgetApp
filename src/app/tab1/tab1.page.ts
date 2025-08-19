2dba764ed03e5332daffef50054164e59bf369ca
Sanele Mpangalala <sanele@rodcode.co.za>
2025-08-19 21:22:33 +0200

    added functionality to view and expense

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Expense } from '../models/expense.model';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { ExpenseComponent } from '../expense/expense.component';
+import { BudgetItem } from '../models/budget.item.model';
 
 @Component({
   selector: 'app-tab1',
@@ -13,7 +14,7 @@ export class Tab1Page {
 
   expenses: Expense[] = [];
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController, private toastCtrl: ToastController) { }
 
   addExpense() {
     this.modalCtrl.create({
@@ -25,11 +26,7 @@ export class Tab1Page {
           amount: 0,
           date: new Date()
         },
-        budgetItems: [
-          { id: 1, description: 'Groceries', amount: 300 },
-          { id: 2, description: 'Transport', amount: 100 },
-          { id: 3, description: 'Entertainment', amount: 150 }
-        ]
+        budgetItems: this.getBudgetItems()
       }
     }).then(modal => {
       modal.present();
@@ -38,10 +35,40 @@ export class Tab1Page {
         if (result.data) {
           this.expenses.push(result.data);
 
-          console.log('Expense added:', result.data);
+          this.toastCtrl.create({
+            message: 'Expense added successfully!',
+            duration: 2000,
+            position: 'bottom'
+          }).then(toast => toast.present());
         }
       });
     });
   }
 
+  viewExpense(expense: Expense) {
+    this.modalCtrl.create({
+      component: ExpenseComponent,
+      componentProps: { expense, budgetItems: this.getBudgetItems() }
+    }).then(modal => {
+      modal.present();
+
+      modal.onDidDismiss().then(result => {
+        if (result.data) {
+          const index = this.expenses.findIndex(e => e.id === expense.id);
+          if (index > -1) {
+            this.expenses[index] = result.data;
+          }
+        }
+      });
+    });
+  }
+
+  getBudgetItems(): BudgetItem[] {
+    return [
+          { id: 1, description: 'Groceries', amount: 300 },
+          { id: 2, description: 'Transport', amount: 100 },
+          { id: 3, description: 'Entertainment', amount: 150 }
+        ]
+  }
 }
+
